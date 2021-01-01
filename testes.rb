f19d31df8341c6662adc5fc0ea3b3667ec4680cd
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-01-01 19:34:01 -0300

    aula-04

--- testes.rb
+++ testes.rb
@@ -1,12 +1,37 @@
-puts 175 == 175
-puts 175 == 174
-puts "175" == 175
-puts "175" == "175"
-puts "175".to_i == 175
+chutes = [176, 100, 130, 150, 175]
+chutes[3] = 300
 
-puts 175 != 174
-puts 175 != 175
-puts 175 > 174
-puts 175 >= 175
-puts 175 < 176
-puts 175 <= 175
+puts chutes[0]
+puts chutes[1]
+puts chutes[2]
+puts chutes[3]
+puts chutes[4]
+
+puts "\n\n"
+
+chutes = []
+
+chute = 176
+tentativa = 1
+chutes[tentativa - 1] = chute
+
+chute = 100
+chutes << chute
+
+chute = 130
+chutes << chute
+
+chute = 150
+chutes << chute
+
+chute = 175
+chutes << chute
+
+for chute in chutes
+    puts chute
+end
+
+nome = gets.strip
+puts "#{nome} tem #{nome.size} caracteres"
+#puts nome + " tem " + nome.size.to_s + " caracteres"
+puts nome[9]
