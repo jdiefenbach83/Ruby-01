d981e6f902aa3c4d328545defbf58c43aebb5837
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2021-01-02 11:56:40 -0300

    aula-06

--- testes.rb
+++ testes.rb
@@ -1,18 +1,4 @@
-treze = 13
-cinco = 5
-puts treze / cinco
-puts treze % cinco
-puts treze + cinco
-puts treze - cinco
-puts treze * cinco
-
-puts "\n"
-
-numero = 5
-numero += 1 # numero = numero + 1 = 6
-numero -= 1 # numero = numero - 1 = 5
-numero *= 2 # numero = numero * 2 = 10
-numero /= 4 # numero = numero / 4 = 2
-numero *=13 # numero = numero * 13 = 26
-numero %=10 # numero = numero % 10 = 6
-puts numero # numero = 6
+for i in 1..10
+    puts "contando..."
+end
+puts i
